3d27859c3085c015e252764322de564465bd3b74
aleotheoreo <aleo.mok@gmail.com>
2018-04-29 13:40:31 -0400

    modified home form

--- helloapp/howdy/forms.py
+++ helloapp/howdy/forms.py
@@ -1,5 +1,24 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
 
+import csv
 
 class NameForm(forms.Form):
-    city_input = forms.CharField(label='City', max_length=100)
+
+    city_input = forms.CharField(label='City', help_text="Enter a city name.", max_length=100, required=True)
+
+    def clean_city_input(self):
+        data = self.cleaned_data['city_input']
+        name = data.lower().split(',')[0] # lower case and remove any reference to state
+        # check if the city is in the csv file
+        valid_city = False
+        with open("dummy_csv.csv", "rb") as f:
+            csvreader = csv.reader(f, delimiter=",")
+            for row in csvreader:
+                if name in row[0]:
+                    valid_city = True
+                    break
+        if not valid_city:
+            raise ValidationError(_('Invalid city name'))
+        return name
--- helloapp/howdy/views.py
+++ helloapp/howdy/views.py
@@ -6,8 +6,54 @@ from django.http import HttpResponseRedirect
 from .forms import NameForm
 
 class HomePageView(TemplateView):
+
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
+
+def getCityName(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            name = form.cleaned_data['city_name']
+            return HttpResponseRedirect('/city_overview/{}'.format(name))
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'name.html', {'form': form})
+
+def lookup_city(request):
+    # If this is a POST request then process the Form data
+    if request.method == 'POST':
+
+        # Create a form instance and populate it with data from the request (binding):
+        form = NameForm(request.POST)
+
+        # Check if the form is valid:
+        if form.is_valid():
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('city-overview/{}'.format(form.cleaned_data['city_input'])) )
+
+    # If this is a GET (or any other method) create the default form.
+    else:
+        form = NameForm(initial={'city_input': 'Enter a city name.',})
+
+    return render(request, 'index.html', {'form': form})
+
+
+
+
+
+
+
+
 
 
 class AboutPageView(TemplateView):
