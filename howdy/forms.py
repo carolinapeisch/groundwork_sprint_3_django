e54b1dcbb69f4e9743c5d39dae24c35bbe9aad05
Carolina Peisch <carolinapeisch@gmail.com>
2018-05-16 09:16:28 -0400

    fixing search page form, small other changes

--- howdy/forms.py
+++ howdy/forms.py
@@ -14,11 +14,11 @@ class NameForm(forms.Form):
         name = data.split(',')[0] # lower case and remove any reference to state
         # check if the city is in the csv file
         valid_city = False
-        fname = os.path.join(os.path.dirname(__file__), 'dummy_csv.csv')
+        fname = os.path.join(os.path.dirname(__file__), 'noopur_real_data.csv')
         with open(fname, "r") as f:
             csvreader = csv.reader(f, delimiter=",")
             for row in csvreader:
-                if name.lower() in row[0].lower():
+                if name.lower() in row[1].lower():
                     valid_city = True
                     break
         if not valid_city:
